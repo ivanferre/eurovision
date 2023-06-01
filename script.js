29681ff40d779ef6d69cba2dd7117e4386e9dd56
Denys Belskiy <denys.belskiy@powercoders.org>
2023-06-01 20:20:48 +0200

    added first version of the script, it works only with the right side of the page

--- script.js
+++ script.js
@@ -1,15 +1,64 @@
-const leftCont = document.querySelector('.mandatory-x');
-const rightCont = document.querySelector('.mandatory-y');
-let amountOfScroll;
+var leftColumn = document.querySelector('.left-fixed'); 
+var rightColumn = document.querySelector('.scroll-content');
+var leftArticles = document.querySelectorAll('.left-fixed article');
+var rightArticles = document.querySelectorAll('.scroll-content article')
 
-rightCont.addEventListener('scroll', event => {
-    preventDefault(event);
-    amountOfScroll = rightCont.scrollTop;
-    leftCont.scrollLeft = amountOfScroll;
+/* Observer Right Column */
+var observerRightColumn = new IntersectionObserver(observerRightColumnCallback, {
+    threshold: 0.2
 });
 
-leftCont.addEventListener('scroll', event => {
-    preventDefault(event);
-    amountOfScroll = leftCont.scrollLeft;
-    rightCont.scrollTop = amountOfScroll;
+function observerRightColumnCallback(entries, observer) {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+         	
+         	let index = Array.prototype.indexOf.call(entry.target.parentNode.children, entry.target);
+            let targetElement = leftArticles[index];
+            let rect = targetElement.getBoundingClientRect();
+			
+			leftColumn.scrollLeft += rect.left;
+      
+        }
+    });
+};
+
+
+rightArticles.forEach((i) => {
+    if (i) {
+        observerRightColumn.observe(i);
+    }
 });
+
+
+
+/* Observer Left Column */
+/*
+var observerLeftColumn = new IntersectionObserver(observerLeftColumnCallback, {
+    threshold: 0.5
+});
+
+
+function observerLeftColumnCallback(entries, observer) {
+    entries.every(entry => {
+        if(entry.isIntersecting) {
+         	
+         	let index = Array.prototype.indexOf.call(entry.target.parentNode.children, entry.target);
+            let targetElement = rightArticles[index];
+            let rect = targetElement.getBoundingClientRect();
+            
+			console.log(index);
+			rightColumn.scrollTop += rect.top;
+      
+						
+        }
+    });
+};
+
+
+leftArticles.forEach((i) => {
+    if (i) {
+        observerLeftColumn.observe(i);
+    }
+});
+*/
+
