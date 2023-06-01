f71d952d4d2848fb25e1f21ac6580b593240b14d
Ivan Ferre <ivan.ferre@powercoders.org>
2023-06-01 14:33:53 +0200

    Add lenis.js

---
+++ lenis.js
@@ -0,0 +1,24 @@
+const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)),
+    orientation: "vertical",
+    gestureOrientation: "vertical",
+    smoothWheel: true,
+    smoothTouch: false,
+    touchMultiplier: 2,
+});
+
+lenis.on('scroll', ({ scroll }) => {
+    console.log(scroll)
+})
+
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+
+function scrollToAnchor(domElement) {
+    lenis.scrollTo(domElement);
+}
+
+requestAnimationFrame(raf);
